8ba5742d4fd1970711eb3e89ed1b908bb73787ec
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2021-04-30 19:48:13 +0200

    fixed field name problem in forklift

--- portacs/src/main/java/it/unipd/threewaymilkshake/portacs/server/engine/clients/Forklift.java
+++ portacs/src/main/java/it/unipd/threewaymilkshake/portacs/server/engine/clients/Forklift.java
@@ -78,7 +78,7 @@ public class Forklift extends Client {
   }
 
   private String getPathToNextTask() {
-    pathToNextTask = map.getPath(position, tasks.getNext());
+    pathToNextTask = warehouseMap.getPath(position, tasks.getNext());
     return pathToNextTask.toString().replaceAll("\\[|\\]", "");
   }
 
